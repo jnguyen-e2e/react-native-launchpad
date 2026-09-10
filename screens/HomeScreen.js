3d2255893e63338f83252e92221c270c9d1be1c2
John Nguyen <72109768+jnguyen-e2e@users.noreply.github.com>
2026-09-10 19:19:47 -0400

    chore(deps): upgrade to Expo SDK 57 and React Native 0.86

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { useMood } from '@/context/mood-context';
 import MoodDisplay from '@/components/MoodDisplay';
